44561aa063878a61e652c2fbfeaabfe47e1f3f16
wendiherrera <karilen_1994@hotmail.com>
2017-06-28 20:40:37 -0700

    Proyecto php

---
+++ Examen2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/bootstrap-3.3.7-dist/css/bootstrap.css"> 
+<style type="text/css">
+	
+</style>
+<title></title>
+</head>
+<body style="background-color:grey;">
+
+<center><div class="alert alert-danger " style="background-color:#1976D2 ; border-bottom-color:#1976D2;  color: white; width:800px;" > 
+	<font face="Comic Sans MS"> Enlace a página de <b>Facebook</b></font></center>
+	</div>	
+	<a href="http://www.facebook.com/">
+ <center><img src="Facebook.png" style="width:125px; height: 125px;" > </center>
+</a>	
+	<br>
+<center><div class="alert alert-info" style="background-color:#81D4FA; border-bottom-color:##81D4FB;  color: white; width:800px;"  >
+	 <font face="Comic Sans MS"> Enlace a página de <b>Twitter<font>
+	</div>
+	<a href="http://www.Twitter.com/">
+ <center><img src="twitter.png" style="width:125px; height: 125px;" > </center>
+</a>	
+	</div>
+
+</html>
